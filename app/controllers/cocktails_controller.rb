389b943fe829a4070159421cd44a94f10823381c
nuno tobias <nmtobias@gmail.com>
2020-02-06 20:53:06 +0000

    controllers

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,22 @@
+class CocktailsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
